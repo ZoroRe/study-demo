999c3d5f7e9914ae92a1cd76b3fc3ce405ca7677
luguanquan <lu_guanquan@163.com>
2022-12-13 14:39:37 +0800

    first init

---
+++ study-demo-service/src/main/java/com/yeswin/study/demo/StudyDemoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yeswin.study.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyDemoServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyDemoServiceApplication.class, args);
+    }
+
+}
---
+++ study-demo-service/src/test/java/com/yeswin/study/demo/StudyDemoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeswin.study.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyDemoServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
